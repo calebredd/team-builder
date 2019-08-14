f89d293cb3f61f98819212c8dd04258a4dc95c25
Caleb Redd <caleb.redd@yahoo.com>
2019-08-13 20:09:40 -0700

    Add names to list for team members

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+
+function Member({ member }) {
+  return <div>{member.name}</div>;
+}
+
+function MemberForm({ addMember }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addMember(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
 
 function App() {
+  const [members, setMembers] = useState([
+    {
+      name: "Jason Bread"
+    },
+    {
+      name: "Wilson Land"
+    },
+    {
+      name: "Brian Deanish"
+    }
+  ]);
+
+  const addMember = name => {
+    const newMember = [...members, { name }];
+    setMembers(newMember);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">Form Management: Team Builder</header>
+    <div className="app">
+      <MemberForm addMember={addMember} />
+      <div className="todo-list">
+        {members.map(member => (
+          <Member member={member} />
+        ))}
+      </div>
     </div>
   );
 }
